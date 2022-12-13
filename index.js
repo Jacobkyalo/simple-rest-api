caa97d6147ed1f41ddfda70a427339fe4933b786
Jacob Kyalo <jacobkyalo43@gmail.com>
2022-12-13 12:19:55 +0300

    cors

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const users = require("./users");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 
 app.get("/users", (req, res) => {
     res.send(users);
